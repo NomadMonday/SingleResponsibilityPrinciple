96317c5975bf5222b60a6c4bb161df92b1605009
NomadMonday <NomadMonday@gmail.com>
2022-01-19 21:35:16 -0800

    Modify end_application message to demonstrate ease of modification.

--- End/standard_messages.py
+++ End/standard_messages.py
@@ -2,7 +2,7 @@ def welcome_message():
     print("Welcome to my application!")
 
 def end_application():
-    input()
+    input("Press enter to close...")
 
 def display_validation_error(field_name):
     print(f"You did not give us a valid {field_name}!")
